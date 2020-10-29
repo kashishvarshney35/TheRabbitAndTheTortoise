dfd890ddaf347041bea57d1d7b3043f98135afc6
kashishvarshney35 <68540149+kashishvarshney35@users.noreply.github.com>
2020-10-29 23:30:28 +0530

    import javax.swing.* in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,4 +1,11 @@
 package thread.tortoise;
 
-public class Tortoise {
+import javax.swing.*;
+
+public class Tortoise extends Thread {
+    private final int MILESTONES = 5;
+    public Tortoise(){
+        super("Tortoise");
+    }
+
 }
